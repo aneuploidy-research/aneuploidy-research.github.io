ef0fd6910c89e68cc13ce40f27c62ad180c72e5d
rmccoy7541 <rajiv.mccoy@gmail.com>
2025-08-14 16:04:08 -0400

    add hamburger menu

--- themes/story/static/js/story.js
+++ themes/story/static/js/story.js
@@ -144,3 +144,15 @@ $( function() {
       });
    }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".hamburger");
+    const menu = document.querySelector(".menu");
+
+    if (hamburger && menu) {
+        hamburger.addEventListener("click", function () {
+            menu.classList.toggle("active");
+        });
+    }
+});
+
